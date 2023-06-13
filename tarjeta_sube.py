6ef6f3fb83480366b9949f23b2fa976963faf84e
franpasquier2 <f.pasquier@alumno.um.edu.ar>
2023-06-13 09:43:48 -0300

    para la tarjeta sube

---
+++ tarjeta_sube.py
@@ -0,0 +1,36 @@
+import unittest
+
+class NoHaySaldoException(Exception):
+    pass
+
+class UsuarioDesactivadoException(Exception):
+    pass
+
+class  EstadoNoExistenteException(Exception):
+    pass
+
+PRIMARIO = "primario"
+SECUNDARIO = "secundario"
+UNIVERSITARIO = "universitario"
+JUBILADO = "jubilado"
+ACTIVADO = "activado"
+DESACTIVADO = "desactivado"
+PRECIO_TICKET = 70
+
+class Sube(self, saldo = 0, grupo_beneficiario = None, estado = "activado"):
+    def __init__(self):
+        self.saldo + saldo
+
+
+
+                
+
+
+    
+
+
+            
+
+
+
+
